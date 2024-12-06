574687bd8c4d2d4670e38dac84228c72db065f18
ajay131619 <pachhinela.ajay@gmail.com>
2024-12-06 13:22:45 +0530

    arrays concept

---
+++ script-5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# name :: ajay
+# date :: Fri, Dec  6, 2024  1:16:53 PM
+
+# script name :: arrays concept
+
+cars=("bmw-m5-cs" "jaguar-ftype" "range rover-sport" "mercedes-g63")
+
+echo "the list of all the cars is : ${cars[@]}"
+echo "the 1st car is ${cars[0]}"
+echo "the 2nd car is ${cars[1]}"
+echo "the 3rd car is ${cars[2]}"
+echo "the 4th car is ${cars[3]}"
