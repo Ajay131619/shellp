e4173dde9daeb2831a9085fac310edb224691781
ajay131619 <pachhinela.ajay@gmail.com>
2024-12-06 10:56:32 +0530

    statements and variables

---
+++ script-2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+name :: ajay
+
+date :: Fri, Dec  6, 2024 10:47:13 AM
+
+script name :: printing statements using variables
+
+here goes the script 
+
+person1=ajay
+person2=saru
+
+echo "${person1} : hi ${person2}!"
+echo "${person2} : hi ${person1}!"
+echo "${person1} : how are you ${person2}!"
+echo "${person2} : i am good ${person1}!"
+echo "${person1} : nice to hear that ${person2}!"
+echo "${person2} : what about you ${person1}!"
+echo "${person1} : i am good ${person2}!"
+echo "${person2} : nice to hear that ${person1}!"
+echo "${person1} : bye ${person2}, i have some work to do !"
+echo "${person2} : bye ${person1}, take care !"
