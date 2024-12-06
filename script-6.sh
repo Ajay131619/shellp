3fe8df0b1e1f5130d3a7787af6e4c4884a4ebdb1
ajay131619 <pachhinela.ajay@gmail.com>
2024-12-06 13:44:11 +0530

    reading and printing vars

---
+++ script-6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# name :: ajay
+
+# date :: Fri, Dec  6, 2024  1:23:33 PM
+
+# script name :: reading variables and printing variables
+
+# here goes the script
+
+# you can also give like this :: 
+
+# echo "enter your name :"
+# read name 
+
+
+
+read -p " please enter your name :: "  name
+
+
+echo " welcome the AJ's script $name "
+
+read -p " please enter your email :: " email 
+
+echo " enter your password :: " 
+read -s password
+
+# here s is used to keep the password or whatever you entered hide 
+
+echo " your email is :: $email"
+echo " your password that you have been entered is :: $password"
+
