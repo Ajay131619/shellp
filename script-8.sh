9a7332ba5abd1524ab4a6d5407217accc189def1
ajay131619 <pachhinela.ajay@gmail.com>
2024-12-06 17:03:57 +0530

    installing sql using linux commands

---
+++ script-8.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# name :: ajay
+
+# date :: Fri, Dec  6, 2024  4:28:45 PM
+
+# script name :: using if-conditions and commands from linux now we are going install a software such as mysql
+
+# here goes the script
+
+userid=$(id -u)
+
+exit_status=$?
+
+#checking the user's id . cuz, In linux only root user can do this kind of tasks
+
+if [ $userid -eq 0 ]
+then
+    echo "now the script is started installing"
+    
+else
+    echo "you are not root user"
+    echo "please run this script with sudo access"
+    exit 1
+fi
+
+dnf list installed mysql
+exit 1
+#checking the software is already installed or not
+
+
+# installation script is below 
+
+if [ $exit_status -eq 0 ]
+then
+    echo "mysql is already installed!!"
+    echo "nothing to do!!"
+    exit 1
+else
+    echo "mysql is not installed!"
+    echo "installation of mysql is initiated!!"
+    dnf install mysql -y
+    if [ $exit_status -eq 0 ]
+    then
+        echo "mysql installed successfully!!"
+        exit 1
+    else
+        echo "mysql installation is failed!!"
+        echo "check it once"
+        exit 1
+    fi
+fi
