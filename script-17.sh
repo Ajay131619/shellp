4c5fd0335d858f6d21d2de1e4e1b9abc2f22b23c
ajay131619 <pachhinela.ajay@gmail.com>
2024-12-20 21:30:15 +0530

    alerting user for disk usage  using scripts

--- script-17.sh
+++ script-17.sh
@@ -3,14 +3,14 @@
 # date :: Fri, Dec 20, 2024  9:19:32 PM
 # script name :: alerting the disk usage using shell script
 
-disk_usage=$(df -HT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" $1f)
+disk_usage=$(df -HT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -1f)
 disk_threshold=20
 disk_name=$(df -HT | grep xfs | awk -F " " '{print $NF}')
 
 echo "date:"$(date)""
 if [ "$disk_usage" -ge "$disk_threshold" ]
 then
-echo "Disk usage is high: $disk_usage%"
+echo "Disk usage is high: "$disk_usage"%"
 echo "Disk name: $disk_name"
 exit 1
 fi
